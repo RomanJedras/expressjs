bb76310e21572691bb4abe11bb2c6c34bf2d057b
Roman Jędras <romanjedras@o2.pl>
2019-06-27 00:10:12 +0200

    Add url to store

--- server.js
+++ server.js
@@ -1,45 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
 const app = express();
-let stringifyFile = null;
 
 
-app.use(bodyParser.json());
 
 
-app.get('/getNote', function(req, res) {
-	fs.readFile('./test.json', 'utf8', function(err, data) {
-		
-		if (err) throw err;
-		stringifyFile = data;
-		res.send(`
-       ${JSON.stringify(data, null, 4)}
-    `);
-	});
-});
-
-
-app.post('/updateNote/:note', function(req, res) {
-	stringifyFile = JSON.stringify(req.params.note, null, 4) ;
-	
-	fs.writeFile('./test.json', stringifyFile, function(err) {
-		if (err) throw err;
-		console.log('file updated');
-		res.send(stringifyFile);
-	});
-	
-});
-
-
-app.use(function (req, res, next) {
-	res.removeHeader('X-Powered-By');
+app.use('/store', function(req, res, next) {
+	console.log('Jestem pośrednikiem przy żądaniu do /store');
 	next();
-	res.locals.data= 'Dane dodane wczesniej'
-	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
 
+app.get('/', function (req, res) {
+	res.send('Hello world!');
+});
+
+app.get('/store', function (req, res) {
+	res.send('To jest sklep');
+});
 
 app.listen(3000);
+
+app.use(function (req, res, next) {
+	res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
+	
+
 
 
